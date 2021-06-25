803cefa32d05767458d75de1e69d0e26788b675f
AmauriBlanco <75027271+AmauriBlanco@users.noreply.github.com>
2021-06-25 12:41:41 -0300

    Update jogoDaVelha.js

--- jogoDaVelha.js
+++ jogoDaVelha.js
@@ -1,15 +1,30 @@
 const boardTable = document.querySelector('.board');
 const gameOverDialog = document.querySelector('.game-over-dialog');
 const gameOverMessage = gameOverDialog.querySelector('.game-over-message');
-const restartButton = gameOverDialog.querySelector('.restart-button');
+const restartButton = gameOverDialog.querySelector('.restart-button',);
+const zerar = document.querySelector('.zerar',);
+
+
+
 
 boardTable.addEventListener('click', setPlayerMove);
 restartButton.addEventListener('click', startGame);
+zerar.addEventListener('click',
+    function(){
+        location.reload();
+    }
+);
+
+
 
 let currentPlayer,
     winner,
     isGameOver,
-    turn;
+    turn,
+    isXWinner,
+    isOWinner;
+    
+
 
 const winningConditions = [
     [0, 1, 2],
@@ -29,9 +44,13 @@ function startGame(){
     winner = null;
     isGameOver = false;
     turn = 0;
+    
     boardTable.style.setProperty('--current-player', '"X"');
     hideGameOverDialog();
     clearBoard();
+    jogadorAtual();
+    
+    
 }
 
 function clearBoard(){
@@ -42,6 +61,7 @@ function setPlayerMove({target}){
     if(!isGameOver && target.classList.contains('cell') && target.innerText === ''){
         target.innerText = currentPlayer;
         turn++;
+        
         if(turn > 4){
             checkGameOver();
         }
@@ -52,24 +72,26 @@ function setPlayerMove({target}){
 function togglePlayer(){
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
     boardTable.style.setProperty('--current-player', `"${currentPlayer}"`);
+    jogadorAtual();
+    
 }
 
 function checkGameOver(){
     winner = checkWinner();
-
     if(winner){
         showGameOverDialog(`Vitória do ${winner}`);
     }else if(turn > 8){
         showGameOverDialog(`Deu Velha!`);
     }
+    placar();
 }
 
 function checkWinner(){
     let cells = boardTable.querySelectorAll('.cell');
     cells = Array.from(cells).map(element => element.innerText);
     const values = winningConditions.map(condition => condition.map(position => cells[position]).join(''));
-    const isOWinner = values.includes('OOO');
-    const isXWinner = values.includes('XXX');
+    isOWinner = values.includes('OOO');
+    isXWinner = values.includes('XXX');
 
     if(isOWinner || isXWinner){
         isGameOver = true;
@@ -79,6 +101,7 @@ function checkWinner(){
         return 'X';
     }
     return null;
+    
 }
 
 function showGameOverDialog(message){
@@ -89,5 +112,21 @@ function showGameOverDialog(message){
 function hideGameOverDialog(){
     gameOverDialog.removeAttribute('open');
 }
+function jogadorAtual(){
+     document.getElementById('player').innerHTML = currentPlayer;
+}
+function placar(){
+    let pontoX = document.getElementById('scoreX');  
+    let pontoO = document.getElementById('scoreO');
+    if(isXWinner === true){
+        pontoX.innerHTML = Number(pontoX.innerHTML) + 1;
+    }
+    if(isOWinner === true){
+        pontoO.innerHTML = Number(pontoO.innerHTML) + 1;
+    }
+    
+
+}
+
 
 startGame();
